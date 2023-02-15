55c5cf9ce1c16fbf02a6fedbeb630cfbbb6e1208
hitesh <hiteshnovember@gamil.com>
2023-02-15 11:14:27 +0530

    Few changes

---
+++ Day_1.py
@@ -0,0 +1,12 @@
+#Lets start
+# print("Hello world!!")
+# print('hello how do you do print("hi")')
+# print("hello " + 'how are you!!')
+# print("hey" + input("how are you"))
+input_1 = int(input("what you want?\n"))
+if input_1 == 1:
+    print("yess")
+else:
+    print("no")
+
+
