160e2788376a1810a28aa57eff37024d72dfb208
hitesh <hiteshnovember@gamil.com>
2023-02-22 11:55:01 +0530

    My__work

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -1,40 +1,60 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z']
-# TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+import Caesar_Cipher_art
 
-def choice(direction):
+#TODO: Choice fuction defined
+def choice(direction,):
     if "encode" == direction:
         encrypt(text,shift)
     elif "decode" == direction:
         decrypt(text,shift)
     else:
         print("Wrong Input")
+
+#TODO: Encryption Function Defined
 def decrypt(text,shift):
     original_message = ''
     for i in range(0,len(text)):
-        if text[i] in alphabet:
-            index = alphabet.index(text[i])
+        if text[i] in Caesar_Cipher_art.alphabet:
+            index = Caesar_Cipher_art.alphabet.index(text[i])
             negative_shift = (index - shift)
             if negative_shift<0:
-                original_message += alphabet[(negative_shift+26)]
+                original_message += Caesar_Cipher_art.alphabet[(negative_shift+26)]
             else:
-                original_message += alphabet[(negative_shift)]
+                original_message += Caesar_Cipher_art.alphabet[(negative_shift)]
         else:
             original_message += text[i]
     print(f"Original Message:{original_message}")
+
+#TODO: Decryption Function Defined
 def encrypt(text,shift):
     # print(f"The plain text message:{text}")
     cipher_txt = ''
     for i in range(0,len(text)):
-       if text[i] in alphabet:
-           index = alphabet.index(text[i])
-           cipher_txt += alphabet[(index+shift)%26]
+       if text[i] in Caesar_Cipher_art.alphabet:
+           index = Caesar_Cipher_art.alphabet.index(text[i])
+           cipher_txt += Caesar_Cipher_art.alphabet[(index+shift)%26]
        else:
            cipher_txt+=text[i]
     print(f"Cipher_txt:{cipher_txt}")
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = list(input("Plain Text:").lower())
-shift = int(input("Type the shift number:"))
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-choice(direction)
+
+#MAIN FUCTION
+print(Caesar_Cipher_art.logo)
+x=False
+while not x:
+    text = list(input("Text:").lower())
+    shift = int(input("Type the shift number:"))
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    choice(direction)
+    input1 = input("To continue,Type Y and N:")
+    if input1=='Y':
+        continue
+    elif input1=='N':
+        break
+
+    else:
+        print("Invalid")
+print("Game Over!!")
+
+
+
+
